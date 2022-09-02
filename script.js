c08a546224b4add21e502d5b6923fb701d29fb37
Jason107206 <53583996+Jason107206@users.noreply.github.com>
2022-09-03 01:53:09 +0800

    Modified: Bug fixes

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
 var img = document.getElementById('img');
-
-var userScore, totalScore, redScore = 0, css;
-var redSora = false;
+var userScore = 0, mode = 0, keyStatus = false, css, audio;
 var audio = new Audio('media/sora_dllm.mp3');
 
 // function
-function down() {
+function init() {
+	readStorage();
+	modeToggle();
+}
+
+function keyDown() {
 	increaseScore();
-	if (redSora) {
+	if (mode > 0) {
 		img.src = 'media/redSora_2.png';
-		
 		audio = new Audio('media/redSora_dllm.mp3');
-		audio.load();
 	} else {
 		img.src = 'media/sora_2.png';
-		
 		audio = new Audio('media/sora_dllm.mp3');
-		audio.load();
 	}
-	
+	audio.load();
 	audio.play();
 }
 
-function up() {
-	if (redSora) {
+function keyUp() {
+	if (mode > 0) {
 		img.src = 'media/redSora_1.png';
 	} else {
 		img.src = 'media/sora_1.png';
@@ -32,59 +31,59 @@ function up() {
 
 function increaseScore() {
     userScore++;
-    totalScore++;
-	redScore++;
-	
-	redSoraToggle();
+	modeToggle();
 	updateDisplay();
 }
 
-function redSoraToggle() {
-	if (redScore == 100) {
-		redSora = !redSora;
-		redScore = 0;
-	}
-	if (redSora) {
-		css = document.querySelector('.text > div > *:first-child');
-		css.style.color = 'rgba(247, 92, 98)';
-		css.innerHTML = "RED";
+function modeToggle() {
+	var r = Math.floor(userScore / 100);	
+	if (userScore - r * 100 === 0) {
+		mode = r - Math.floor(r / 2) * 2;
 		
-		css = document.querySelector('body');
-		css.style.background = 'linear-gradient(to right, rgb(253, 178, 172), rgb(255, 223, 221))';
-		
-		css = document.querySelector('.dialog');
-		css.style.borderColor = 'rgb()';
-		css.style.backgroundColor = 'rgb()';
-		
-		css = document.querySelectorAll(".dialog > *");
-		for (let i = 0; i < css.length; i++) {
-			css[i].style.color = 'rgb()';
+		if (mode === 0) {
+			css = document.querySelector('.text > div > *:first-child');
+			css.style.color = 'rgb(255, 163, 183)';
+			css.innerHTML = "ZIZI";
+			
+			css = document.querySelector('body');
+			css.style.background = 'linear-gradient(to right, rgb(250, 204, 214), rgb(255, 244, 248))';
+			
+			css = document.querySelector('.dialog');
+			css.style.borderColor = 'rgb(244, 171, 182)';
+			css.style.backgroundColor = 'rgb(254, 217, 224)';
+			
+			css = document.querySelectorAll(".dialog > *");
+			for (let i = 0; i < css.length; i++) {
+				css[i].style.color = 'rgb(244, 171, 182)';
+			}
+			
+			css = document.querySelectorAll(".dialog > * > *:first-child");
+			for (let i = 0; i < css.length; i++) {
+				css[i].style.color = 'rgb(255, 118, 153)';
+			}
 		}
 		
-		css = document.querySelectorAll(".dialog > * > *:first-child");
-		for (let i = 0; i < css.length; i++) {
-			css[i].style.color = 'rgb()';
-		}
-	} else {
-		css = document.querySelector('.text > div > *:first-child');
-		css.style.color = 'rgb(255, 163, 183)';
-		css.innerHTML = "ZIZI";
-		
-		css = document.querySelector('body');
-		css.style.background = 'linear-gradient(to right, rgb(250, 204, 214), rgb(255, 244, 248))';
-		
-		css = document.querySelector('.dialog');
-		css.style.borderColor = 'rgb(244, 171, 182)';
-		css.style.backgroundColor = 'rgb(254, 217, 224)';
-		
-		css = document.querySelectorAll(".dialog > *");
-		for (let i = 0; i < css.length; i++) {
-			css[i].style.color = 'rgb(243, 163, 190)';
-		}
-		
-		css = document.querySelectorAll(".dialog > * > *:first-child");
-		for (let i = 0; i < css.length; i++) {
-			css[i].style.color = 'rgb(255, 118, 153)';
+		if (mode === 1) {
+			css = document.querySelector('.text > div > *:first-child');
+			css.style.color = 'rgba(247, 92, 98)';
+			css.innerHTML = "RED";
+			
+			css = document.querySelector('body');
+			css.style.background = 'linear-gradient(to right, rgb(253, 178, 172), rgb(255, 223, 221))';
+			
+			css = document.querySelector('.dialog');
+			css.style.borderColor = 'rgb(239, 148, 148)';
+			css.style.backgroundColor = 'rgb(255, 198, 198)';
+			
+			css = document.querySelectorAll(".dialog > *");
+			for (let i = 0; i < css.length; i++) {
+				css[i].style.color = 'rgb(239, 148, 148)';
+			}
+			
+			css = document.querySelectorAll(".dialog > * > *:first-child");
+			for (let i = 0; i < css.length; i++) {
+				css[i].style.color = 'rgb(240, 106, 108)';
+			}
 		}
 	}
 }
@@ -94,16 +93,11 @@ function updateDisplay() {
 	for (let i = 0; i < css.length; i++) {
 		css[i].innerHTML = userScore;
 	}
-	
-	// css = document.querySelector('.dialog > *:last-child > *:last-child');
-	// css[i].innerHTML = totalScore;
 }
 
 function readStorage() {
 	if (localStorage.getItem('userScore')) {
 		userScore = parseFloat(localStorage.getItem('userScore'));
-		totalScore = parseFloat(0);
-		totalScore += userScore;
 		updateDisplay();
 	}
 }
@@ -114,59 +108,56 @@ function writeStorage() {
 
 function clear() {
 	localStorage.clear();
-	
 	userScore = 0;
-	totalScore = 0;
-	redScore = 0;
-	redSora = false;
 	
-	redSoraToggle();
+	modeToggle();
 	updateDisplay();
 	
 	img.src = 'media/sora_1.png';
-	
 	return 'Success';
 }
 
 function lastUpdate() {
-	return '4:10 22/4/2022';
+	return '1:51 3/9/2022';
 }
 
 // event
 
 img.onmousedown = e => {
 	e.preventDefault();
-	down();
+	keyDown();
 }
     
 img.onmouseup = e => {
 	e.preventDefault();
-	up();
+	keyUp();
 }
 
 img.ontouchstart = e => {
 	e.preventDefault();
-	down();
+	keyDown();
 }
     
 img.ontouchend = e => {
 	e.preventDefault();
-	up();
+	keyUp();
 }
 
 window.onkeydown = e => {
-	if (e.keyCode === 13) {
-		down();
+	if (e.keyCode === 13 && keyStatus == false) {
+		keyDown();
+		keyStatus = true;
 	}
 }
 
 window.onkeyup = e => {
 	if (e.keyCode === 13) {
-		up();
+		keyUp();
+		keyStatus = false;
 	}
 }
 
-window.addEventListener("load", readStorage);
+window.addEventListener("load", init);
 
 window.addEventListener("pagehide", writeStorage);
 
